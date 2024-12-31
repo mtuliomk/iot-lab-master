fc31db99ccc199e6016e057f5f566523d42c487d
Marco Tulio <marco.korehisa@escale.com.br>
2024-12-31 10:09:12 -0300

    novo dispositivo

--- tulio-iot-central.ino
+++ tulio-iot-central.ino
@@ -1,5 +1,6 @@
 #define MQTT_MAX_PACKET_SIZE 1024
 
+#include <stdint.h>
 #include <RCSwitch.h>
 #include <ArduinoJson.h>
 
@@ -16,7 +17,35 @@ const char* password = "F54$iatr*2863";
 const char* mqttServer = "a2hrfsgkuw6dwa-ats.iot.us-east-1.amazonaws.com";
 const int mqttPort = 8883;
 
-bool isON = false;
+uint16_t thingStates = 0b0000000000000000;  // Inicialmente, todos os 16 bits desligados
+struct ThingInfo {
+  String slug;
+  uint8_t bitPosition;
+  int frequency;
+};
+ThingInfo things[] = {
+  { "FirstTestDevice", 0, 2565608 },
+  { "SecondTestDevice", 1, 6460648 }
+};
+
+ThingInfo* getThingInfo(String slug) {
+  for (int i = 0; i < sizeof(things) / sizeof(things[0]); i++) {
+    if (things[i].slug == slug) {
+      return &things[i];  // Retorna o endereço da ThingInfo encontrada
+    }
+  }
+  return nullptr;  // Retorna nullptr se não encontrar
+}
+
+ThingInfo* getThingInfo(int frequency) {
+  for (int i = 0; i < sizeof(things) / sizeof(things[0]); i++) {
+    if (things[i].frequency == frequency) {
+      return &things[i];  // Retorna o endereço da ThingInfo encontrada
+    }
+  }
+  return nullptr;  // Retorna nullptr se não encontrar
+}
+
 const int ledPin = 2;  // GPIO do LED interno
 
 // Inicializa o cliente MQTT
@@ -45,21 +74,21 @@ void subscribeTopic(String topic) {
   String deltaTopic = "$aws/things/" + topic + "/shadow/update/delta";
   String getTopic = "$aws/things/" + topic + "/shadow/get";
 
-  if (client.subscribe(acceptedTopic.c_str())) { 
+  if (client.subscribe(acceptedTopic.c_str())) {
     Serial.println("Inscrito no tópico accepted");
   } else {
     Serial.println("Falha ao se inscrever no tópico accepted");
   }
   delay(100);
 
-  if (client.subscribe(deltaTopic.c_str())) { 
+  if (client.subscribe(deltaTopic.c_str())) {
     Serial.println("Inscrito no tópico delta");
   } else {
     Serial.println("Falha ao se inscrever no tópico delta");
   }
   delay(100);
 
-  if (client.publish(getTopic.c_str(), "{}")) { 
+  if (client.publish(getTopic.c_str(), "{}")) {
     Serial.println("Solicitação publicada no tópico get");
   } else {
     Serial.println("Falha ao publicar no tópico get");
@@ -103,15 +132,18 @@ void callback(char* topic, byte* payload, unsigned int length) {
     powerState = doc["state"]["powerState"] | "";
   }
 
-  if (powerState.equals("ON")) {
-    isON = true;
-  }
-  if (powerState.equals("OFF")) {
-    isON = false;
+  String slug;
+  int start = String(topic).indexOf("things/") + 7;
+  int end = String(topic).indexOf("/shadow");
+  if (start > 0 && end > start) {
+    slug = String(topic).substring(start, end);
   }
 
-  Serial.print("isON: ");
-  Serial.println(isON);
+  Serial.print("Mudando o status de: ");
+  Serial.println(slug);
+
+  ThingInfo* thing = getThingInfo(slug);
+  thingStates ^= (1 << thing->bitPosition);  // alterna o status
 }
 
 void loop() {
@@ -126,6 +158,7 @@ void loop() {
     if (receivedValue == 0) {
       Serial.println("Nenhum código recebido.");
     } else {
+      Serial.println(receivedValue);
       switchPressed(receivedValue);
     }
     mySwitch.resetAvailable();
@@ -133,14 +166,24 @@ void loop() {
 }
 
 void switchPressed(int code) {
-  if (code == 6460648) {
-    Serial.print("Código recebido: ");
-    Serial.println(code);
-    if (isON) {
-      publishToThing(client, "SecondTestDevice", "OFF");
+  Serial.print("Código recebido: ");
+  Serial.println(code);
+  switchThing(code);
+  delay(2000);
+}
+
+void switchThing(int receivedFrequency) {
+  ThingInfo* thing = getThingInfo(receivedFrequency);
+  if (thing != nullptr) {
+    Serial.print("Pressionando ");
+    Serial.println(thing->slug);
+
+    if (thingStates & (1 << thing->bitPosition)) {  // ligado
+      publishToThing(client, thing->slug.c_str(), "OFF");
+      Serial.println("Desligando");
     } else {
-      publishToThing(client, "SecondTestDevice", "ON");
+      publishToThing(client, thing->slug.c_str(), "ON");
+      Serial.println("Ligando");
     }
-    delay(2000);
   }
 }
